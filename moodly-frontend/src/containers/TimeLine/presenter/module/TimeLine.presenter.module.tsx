1d2c72419c8419f58b3a1b5b0c2d7182e888610c
juaovo <a01068020264@gmail.com>
2026-07-11 20:16:30 +0900

    timeline html

--- moodly-frontend/src/containers/TimeLine/presenter/module/TimeLine.presenter.module.tsx
+++ moodly-frontend/src/containers/TimeLine/presenter/module/TimeLine.presenter.module.tsx
@@ -1,5 +1,6 @@
 import styles from './TimeLine.module.css';
 import classNames from 'classnames/bind';
+import Image from 'next/image';
 import {type FC} from 'react';
 
 const cx = classNames.bind(styles);
@@ -8,10 +9,13 @@ const TimeLinePresenterModule: FC = () => {
   return (
     <div className={cx('timeline')}>
       <h1 className={cx('year')}>2026</h1>
-      <div className={cx('timeline__item')}></div>
-      <div className={cx('timeline__item__content')}>
-        <h2 className={cx('timeline__item__title')}>4월 20일</h2>
-        <p className={cx('timeline__item__description')}>히히 블로그에 지금까지 썼던 감정일기를 올렸다!</p>
+      <div className={cx('timeline__item')}>
+        <Image src="/images/calm.png" alt="calm" width={50} height={50} className={cx('timeline__item__image')} />
+        <div className={cx('timeline__item__content')}>
+          <h2 className={cx('timeline__item__date')}>4월 20일</h2>
+          <p className={cx('timeline__item__day')}>일요일</p>
+          <p className={cx('timeline__item__description')}>히히 블로그에 지금까지 썼던 감정일기를 올렸다!</p>
+        </div>
       </div>
     </div>
   );
